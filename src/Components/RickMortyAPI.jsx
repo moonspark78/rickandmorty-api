212fe7e4662ead20e89f4c429c5cad12eacac99e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-10 06:34:03 +0100

    :construction: create the useState and useEffect  :construction:

--- src/Components/RickMortyAPI.jsx
+++ src/Components/RickMortyAPI.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const RickMortyAPI = () => {
+    const [infoData, setInfoData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {};
+    },[])
+
+
+
+
+
   return (
     <div>RickMortyAPI</div>
   )
