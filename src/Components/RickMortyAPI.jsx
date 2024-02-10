03f28076194f6a39400357e1bc29cc0088361165
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-10 06:51:31 +0100

    :rocket: :tada: display image of the characters  :rocket: :tada:

--- src/Components/RickMortyAPI.jsx
+++ src/Components/RickMortyAPI.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import "./styles.css"
 
 export const RickMortyAPI = () => {
     const [infoData, setInfoData] = useState([]);
@@ -8,6 +9,7 @@ export const RickMortyAPI = () => {
             const response = await fetch("https://rickandmortyapi.com/api/character");
             const data = await response.json();
             console.log(data.results);
+            setInfoData(data.results);
         };
         fetchData();
     },[])
@@ -19,6 +21,16 @@ export const RickMortyAPI = () => {
   return (
     <div>
         <h2>RickMortyAPI</h2>
+        <div className='card'>
+            {
+                infoData.map(info =>(
+                    <div key={info.id}>
+                        <p>{info.name}</p>
+                        <img src={info.image} alt='photo'/>
+                    </div>
+                ))
+            }
+        </div>
     </div>
   )
 }
