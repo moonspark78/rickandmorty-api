f8156234c88202bdbfbb123f5f14737c24cb790b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-10 06:11:57 +0100

    :construction: create the components folder  :construction:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {RickMortyAPI} from "./Components/RickMortyAPI"
+
+function App() {
+  return (
+    <div className="App">
+      <RickMortyAPI/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/RickMortyAPI.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RickMortyAPI = () => {
+  return (
+    <div>RickMortyAPI</div>
+  )
+}
